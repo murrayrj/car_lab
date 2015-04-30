1e9e2cf2d1bc180be7146b81af0be269beaf9526
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-04-30 17:12:22 +0100

    #currentOwner fixed

--- src/car.js
+++ src/car.js
@@ -1,10 +1,11 @@
-function Car(make, model, year, color, state, previousOwners){
+function Car(make, model, year, color, state, previousOwners, currentOwner){
   this.make = make;
   this.model = model;
   this.year = year;
   this.color = color;
   this.state = 'off';
   this.previousOwners = '';
+  this.currentOwner = 'Manufacturer';
 }
 
 Car.prototype.sale = function(newOwner){
