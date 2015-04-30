2c1f9b4e9c89093c9dd5752ed0e23ab86fdc54d8
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-04-30 18:02:35 +0100

    #pickUp[2] fixed

--- src/car.js
+++ src/car.js
@@ -27,7 +27,8 @@ this.state ='off';
 };
 
 Car.prototype.pickUp = function(passengers){
-this.passengers =['john'];
+if (this.state === 'on'){
+this.passengers =['john']
 };
-
+}
 module.exports=Car;
