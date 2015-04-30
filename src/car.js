68ad33f2e1620b0bdd33d22db320dc3165409a2f
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-04-30 17:09:28 +0100

    #previousOwners fixed

--- src/car.js
+++ src/car.js
@@ -1,9 +1,10 @@
-function Car(make, model, year, color, state){
+function Car(make, model, year, color, state, previousOwners){
   this.make = make;
   this.model = model;
   this.year = year;
   this.color = color;
   this.state = 'off';
+  this.previousOwners = '';
 }
 
 Car.prototype.sale = function(newOwner){
